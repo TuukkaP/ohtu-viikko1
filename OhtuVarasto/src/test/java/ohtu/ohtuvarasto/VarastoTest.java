6397384e5d08133d12e1185502814984a3810832
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-03-13 23:16:58 +0200

    Testikommentti

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class VarastoTest {
-// TÄNNE TULI UUSI KOMMENTTI.
+// TÄNNE TULI UUSI KOMMENTTI. KORJAUS
     Varasto varasto;
     Varasto negvarasto;
     double vertailuTarkkuus = 0.0001;
