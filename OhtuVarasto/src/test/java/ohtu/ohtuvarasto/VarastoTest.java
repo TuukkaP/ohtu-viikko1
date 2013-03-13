2650327806fe52fb8825347e44572a6c8de0b5ac
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-03-13 22:26:45 +0200

    Rikotaan testi Jenkinsia varten

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -66,7 +66,7 @@ public class VarastoTest {
         varasto.lisaaVarastoon(8);
 
         // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
-        assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(1, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
     @Test
